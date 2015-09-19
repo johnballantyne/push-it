c5e52b3b8759bcfa695395b41b0060389d91658b
johnballantyne <johnballantyne@gmail.com>
2015-09-18 21:21:31 -0400

    Add client video hook

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,15 @@
 var socket = io();
+var vid = document.getElementById('bigvid');
+
+setInterval(function () { console.log(vid.buffered.end(0)) }, 17);
+
 
 socket.on('push it', function (data) {
-    document.getElementsByTagName('video')[0].play();
     console.log('Pushed.');
+    vid.pause();
+    vid.currentTime = 0;
+    vid.muted = false;
+    vid.style.visibility = "inherit";
+    vid.play();
 });
 
