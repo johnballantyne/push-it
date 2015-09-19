03901d046c37ef809cb677d24be6412722afe9fe
johnballantyne <johnballantyne@gmail.com>
2015-09-19 19:55:36 -0400

    Add custom namespace, dynamic rooms

--- app.js
+++ app.js
@@ -2,24 +2,42 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var pushitio = io.of('/push-it');
 var github = require('./github');
 
 app.use(express.static('public'));
-
-io.on('connection', function(socket){
-    github.setActive(true);
-    github.getCommits(function annoyEveryone() {
-        io.emit('push it', 'test');
-    });
-    console.log('User conn');
-    socket.on('disconnect', function () {
-        github.setActive(false);
-    });
+// TODO: Dynamic URIs
+app.get('/johnballantyne/fbhw', function (req, res) {
+    res.sendFile('index.html', { 'root': __dirname + '/public/' });
 });
 
-io.on('disconnect', function(socket) {
-    console.log('---Disconnect');
-    github.setActive(false);
+app.get('/dw', function (req, res) {
+    res.download(__dirname + '/public/doctor_who_2005.9x01.the_magicians_apprentice.720p_hdtv_x264-fov.mkv');
+});
+var clients = {};
+
+function updateClients(clientData) {
+    pushitio.emit('clients update', clients);
+}
+
+
+pushitio.on('connection', function(socket){
+    github.setActive(true);
+    github.getCommits(function annoyEveryone() {
+        pushitio.emit('push it', 'test');
+    });
+
+    socket.on('client connected', function (data) {
+        console.log('Joining: %s', data.room);
+        socket.join(data.room);
+        console.log('Our rooms: %s', pushitio.rooms);
+        updateClients(data);
+        console.log("clients: " + clients);
+    });
+
+    socket.on('disconnect', function (data) {
+        github.setActive(false);
+    });
 });
 
 http.listen(3000, function(){
--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,34 @@
-var socket = io();
+var socket = io('/push-it');
+var fingerprint2str;
 var vid = document.getElementById('bigvid');
+var clients = [];
 
-setInterval(function () { console.log(vid.buffered.end(0)) }, 17);
+fingerprint2str = localStorage.getItem('fingerprint2');
+var uri = document.createElement('a');
+uri.href = document.URL;
 
+socket.on('connect', function () {
+    console.log('Firing. Room: %s', uri.pathname);
+    if (!localStorage.getItem('fingerprint2')) {
+        console.log('No Fingerprint found. Setting...');
+        new Fingerprint2().get(function (result) {
+            fingerprint2str = result;
+            localStorage.setItem('fingerprint2', result);
+            socket.emit('client connected', {
+                'fp2str': fingerprint2str,
+                'room': uri.pathname
+            });
+        });
+    } else {
+        socket.emit('client connected', {
+            'fp2str': fingerprint2str,
+            'room': uri.pathname
+        });
+    }
+});
+
+socket.on('disconnect', function () {
+});
 
 socket.on('push it', function (data) {
     console.log('Pushed.');
@@ -13,3 +39,7 @@ socket.on('push it', function (data) {
     vid.play();
 });
 
+socket.on('clients update', function (data) {
+    console.log('Clients:');
+    console.log(data);
+});
